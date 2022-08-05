41833a95d6357f290628f3aadc974523b95ef264
Kyle Chinick <kylechinick@icloud.com>
2022-08-05 09:49:32 -0700

    Add migration with mvp fields

---
+++ Factory/Migrations/20220805164831_MvpFields.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class MvpFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Machines",
+                newName: "MachineName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MachineName",
+                table: "Machines",
+                newName: "Name");
+        }
+    }
+}
